9c8b1ed6da078b53b1b786308351a0d261315e2d
unknown <r0714351@student.thomasmore.be>
2019-10-25 09:08:46 +0200

    Opdracht 3: <= Eloquent models

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,6 +2,7 @@
 @section('title', 'Contact info')
 
 @section('main')
-<p>The Vinyl Shop</p>
-<p><a href="mailto:info@thevinylshop.com">info@thevinylshop.com</a></p>
+    <h1>Contact Info</h1>
+    <p>The Vinyl Shop</p>
+    <p><a href="mailto:info@thevinylshop.com">info@thevinylshop.com</a></p>
 @endsection
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -24,8 +24,5 @@
 {{--  Footer  --}}
 @include("shared.footer")
 <script src="{{ mix('js/app.js') }}"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
